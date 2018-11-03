c408dc7f33b39b6977f5e3b17ed38e26154552a7
cclausen <colin_clausen@ki.tng.de>
2018-11-03 14:45:36 +0100

    verteiltung der vorkommen

--- wordcount.js
+++ wordcount.js
@@ -6,7 +6,7 @@ function escapeRegExp(string) {
 function WordCountModel() {
     var self = this;
 
-    self.ignoreChars = ko.observable("‚’‘0123456789,;+.:_-!'#„“\"§$%&/\\()?\}][{@€<>|=*^");
+    self.ignoreChars = ko.observable("”‚’‘0123456789,;+.:_-!'#„“\"§$%&/\\()?\}][{@€<>|=*^©–");
 
     self.inputText = ko.observable("");
 
@@ -22,6 +22,8 @@ function WordCountModel() {
 
     self.toplist = ko.observable([]);
 
+    self.countHistogramData = ko.observable([]);
+
     self.sortByAlpha = function() {
         var result = self.toplist();
 
@@ -32,9 +34,7 @@ function WordCountModel() {
         self.toplist(result);
     };
 
-    self.sortByCount = function() {
-        var result = self.toplist();
-
+    var sortListByCount = function(result) {
         result = _.sortBy(result, function(v) {
             return v.word;
         });
@@ -42,25 +42,21 @@ function WordCountModel() {
         result = _.sortBy(result, function(v){
             return -v.count;
         });
+        return result;
+    };
+
+    self.sortByCount = function() {
+        var result = self.toplist();
+
+        result = sortListByCount(result);
 
         self.toplist(result);
     };
 
-    self.process = function() {
-        self.toplist([]);
-        self.charLength(self.inputText().length);
-        var filteredIgnoreChars = filterIgnoreChars(self.inputText());
-        var rdyForProc = filteredIgnoreChars.toLowerCase();
-
-        var words = rdyForProc.split(/\s+/);
-        
-        self.wordLength(words.length);
-
+    var countSet = function(words) {
         var counts = Object.create(null);
 
         for (var i = 0; i < words.length; i++) {
-            if (words[i].length == 0) continue;
-
             if (!counts[words[i]]) {
                 counts[words[i]] = 1;
             } else {
@@ -74,7 +70,38 @@ function WordCountModel() {
             result.push({'word': w, 'count': counts[w]});
         }
 
+        return result;
+    };
+
+    self.process = function() {
+        self.toplist([]);
+        self.charLength(self.inputText().length);
+        var filteredIgnoreChars = filterIgnoreChars(self.inputText());
+        var rdyForProc = filteredIgnoreChars.toLowerCase();
+
+        var wordSplit = rdyForProc.split(/\s+/);
+        
+        var words = [];
+
+        for (var i = 0; i < wordSplit.length; i++) {
+            if (wordSplit[i].length > 0) words.push(wordSplit[i]);
+        }
+
+        self.wordLength(words.length);
+
+        var result = countSet(words);
+
         self.uniqueWords(result.length);
+
+        var countHisto = countSet(_.map(result, function(e) {return e.count}));
+
+        countHisto = _.map(countHisto, function(e) {
+            return {'word': Number(e.word), 'count': e.count};
+        });
+
+        countHisto = sortListByCount(countHisto);
+
+        self.countHistogramData(countHisto);
 
         self.toplist(result);
 
@@ -89,27 +116,42 @@ function WordCountModel() {
         return result;
     }
 
-    self.saveResult = function() {
-        var newLine = "\r\n";
-        var txt = "Zeichen verarbeitet: " + self.charLength() + newLine;
-        txt += "Wörter verarbeitet: " + self.wordLength()+ newLine;
-        txt += "Einzigartige Wörter: " + self.uniqueWords() + newLine;
-        txt += newLine;
+    var newLine = "\r\n";
 
+    var mkTxt = function(lst) {
+        var txt = "";
         var longestWord = 0;
         var longestNum = 0;
 
-        for (var i = 0; i < self.toplist().length; i++) {
-            var wl = self.toplist()[i].word.length;
-            var nl = (self.toplist()[i].count + "").length;
+        for (var i = 0; i < lst.length; i++) {
+            var wl = lst[i].word.length;
+            var nl = (lst[i].count + "").length;
             
             if (wl > longestWord) longestWord = wl;
             if (nl > longestNum) longestNum = nl;
         }
 
-        for (var i = 0; i < self.toplist().length; i++) {
-            txt += rightPad(self.toplist()[i].word, longestWord) + "   " + rightPad(self.toplist()[i].count, longestNum) + "   #" + (i+1) + newLine;
+        for (var i = 0; i < lst.length; i++) {
+            txt += rightPad(lst[i].word, longestWord) + "   " + rightPad(lst[i].count, longestNum) + "   #" + (i+1) + newLine;
         }
+
+        return txt;
+    };
+
+    self.saveResult = function() {
+        var txt = "Zeichen verarbeitet: " + self.charLength() + newLine;
+        txt += "Wörter verarbeitet: " + self.wordLength()+ newLine;
+        txt += "Einzelne Wörter: " + self.uniqueWords() + newLine;
+        txt += newLine;
+        txt += "Verteilung der Anzahlen" + newLine;
+
+        txt += mkTxt(self.countHistogramData());
+
+        txt += newLine;
+        txt += "Topliste der Wörter" + newLine;
+
+        txt += mkTxt(self.toplist());
+
         saveAs(new Blob([txt], {type: "application/text"}), "export.txt");
     };
 }
